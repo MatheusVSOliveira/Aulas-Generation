23602f64de970ae4041d1095f9f01be1b55b7ff3
Matheus Oliveira <matheusvsoliveira61@gmail.com>
2021-10-01 17:41:17 -0300

    Atividade CRUD/API rest

---
+++ Springboot/lojafarmacia/src/main/java/br/org/generation/lojafarmacia/model/Produto.java
@@ -0,0 +1,116 @@
+package br.org.generation.lojafarmacia.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 2, max = 255, message = "preenchimento do campo nome obrigatório")
+	private String nome;
+	
+	@Min(0)
+	private int qtdEstoque;
+	
+	@NotNull
+	private LocalDate vencimento;
+	
+	@NotNull(message = "valor é obrigatório!")
+	@Positive(message = "o valor deve ser maior que ZERO!")
+	private BigDecimal valor;
+	
+	@Size(max = 500)
+	private String descricao;
+	
+	@NotNull
+	private boolean receita;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getQtdEstoque() {
+		return qtdEstoque;
+	}
+
+	public void setQtdEstoque(int qtdEstoque) {
+		this.qtdEstoque = qtdEstoque;
+	}
+
+	public LocalDate getVencimento() {
+		return vencimento;
+	}
+
+	public void setVencimento(LocalDate vencimento) {
+		this.vencimento = vencimento;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public boolean getReceita() {
+		return receita;
+	}
+
+	public void setReceita(boolean receita) {
+		this.receita = receita;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+}
---
+++ Springboot/lojafarmacia/src/test/java/br/org/generation/lojafarmacia/LojafarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.generation.lojafarmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojafarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
