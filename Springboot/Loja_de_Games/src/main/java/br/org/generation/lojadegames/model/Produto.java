fc91a532c0eb84cd6ab778f38a93fd548cffd9bf
Matheus Oliveira <matheusvsoliveira61@gmail.com>
2021-10-01 15:35:52 -0300

    Atividade CRUD/API rest

---
+++ Springboot/Loja_de_Games/src/main/java/br/org/generation/lojadegames/model/Produto.java
@@ -0,0 +1,85 @@
+package br.org.generation.lojadegames.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 2, max = 255)
+	private String nome;
+	
+	@NotNull
+	@Min(0)
+	private int quantidade_estoque;
+	
+	@NotNull
+	@Positive
+	@Digits(integer  = 3,fraction = 2, message = "O preço do jogo só pode ser uma centena")
+	private BigDecimal valor;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getQuantidade_estoque() {
+		return quantidade_estoque;
+	}
+
+	public void setQuantidade_estoque(int quantidade_estoque) {
+		this.quantidade_estoque = quantidade_estoque;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+}
